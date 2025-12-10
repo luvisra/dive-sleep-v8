2ba422f39f7a69edfc1814a44491ec38810bed8a
Gilbert <luvisra@gmail.com>
2025-12-10 11:00:03 +0900

    adding moblile platforms.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kr.cnfrontier.sleep',
+  appName: 'DIVE 슬립',
+  webDir: 'www'
+};
+
+export default config;
